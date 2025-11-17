7e010f0e5c13838cf0e369ba531f0477539d5f2b
Huanyao <huanyao.gao@yale.edu>
2025-11-16 19:49:29 -0800

    Updated bitor json file from csv source to tsv source

---
+++ .gitignore/Git_push.sh
@@ -0,0 +1,13 @@
+#git pull
+
+# 1. Check what’s changed
+git status
+
+# 2. Stage all changes (modified, added, deleted files)
+git add .
+
+# 3. Commit with a message describing the update
+git commit -m "Describe your changes here"
+
+# 4. Push to GitHub (main branch)
+git push origin main
