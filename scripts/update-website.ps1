4727a0b2bd09a3fd83ecf755e3d40d2bdb1899a9
Felix <ecybermission796@gmail.com>
2025-11-23 13:46:11 -0800

    Update plant and animal pictures

--- scripts/update-website.ps1
+++ scripts/update-website.ps1
@@ -1,5 +1,6 @@
 PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\convert-biters-csv.ps1 -csv .\data\Biterdata.csv -out .\data\Biterdata.json
 #PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\convert-biters-txt.ps1 -txt .\data\Biterdata.txt -out .\data\Biterdata.json
-PowerShell.exe -File .\scripts\generate-animals-manifest.ps1
-PowerShell.exe -File .\scripts\generate-plants-manifest.ps1
-PowerShell.exe -File .\scripts\generate-bugs-manifest.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-animals-manifest.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-plants-manifest.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-bugs-manifest.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\scripts\check-manifest-names.ps1
