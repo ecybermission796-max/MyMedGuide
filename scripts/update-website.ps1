846a81f8eab99a879e610ea7fb3dc18a8479e940
ecybermission796-max <ecybermission796-max@users.noreply.github.com>
2025-12-01 03:28:12 +0000

    deploy: ff6c8464344d78e7bd9a0990dcceadfa5c6bee4c

--- scripts/update-website.ps1
+++ scripts/update-website.ps1
@@ -1,6 +1,6 @@
 #powershell -NoProfile -Command "Import-Csv '.\.gitignore\rawfiles\Biterdata.csv' | Where-Object Section -match 'class' | Select-Object Keywords,Section,Title,Description | Format-Table -AutoSize"
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File .\scripts\convert-biters-txt.ps1 -txt .\.gitignore\rawfiles\Biterdata.txt -out .\data\Biterdata.json
-# PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-animals-manifest.ps1
-# PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-plants-manifest.ps1
-# PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-bugs-manifest.ps1
-# powershell.exe -ExecutionPolicy Bypass -File .\scripts\check-manifest-names.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-animals-manifest.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-plants-manifest.ps1
+PowerShell.exe -ExecutionPolicy Bypass -File .\scripts\generate-bugs-manifest.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\scripts\check-manifest-names.ps1
